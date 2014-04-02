2c5a23370e013910800fc222895333714fb1714f
hxshandle <handle.huang0409@gmail.com>
2014-04-02 22:33:51 +0800

    landing page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-$(function(){
-  $(".img-liquid-fill").imgLiquid();
+var WW, WH;
+function landingLayout() {
+	$('.landing-section').css({
+		height: WH + 'px'
+	});
+}
+
+function doResize() {
+	WW = $(window).width();
+	WH = $(window).height();
+	landingLayout();
+}
+
+$(function() {
+	$(".img-liquid-fill").imgLiquid();
+	$(".img-liquid-fill-top").imgLiquid({verticalAlign:'top'});
+	doResize();
+	$(window).resize(doResize);
 });
+
